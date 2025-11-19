f7a722dfd324073ffc618d032ebddcf48f9174f6
Jawad Hafeez <jawadhafeez171@gmail.com>
2025-11-19 14:35:24 +0530

    logo

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { COMPANY_NAME, ADDRESS, PHONE_NUMBER, SERVICES, SOCIAL_LINKS } from '../constants';
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,4 @@
+
 import React, { useState, useEffect } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import { COMPANY_NAME, PHONE_NUMBER } from '../constants';
